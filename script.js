5350c93ec13ccd77b3e89c602150d9d5d0383d5e
Roland HUON <huonroland91@gmail.com>
2025-06-23 16:54:48 +0200

    base js

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ canvas.height = height;
 
 const context = canvas.getContext('2d');
 
+const drawPlanet = () => {
+    context.beginPath();
+    context.arc(canvas.width/2, canvas.height, 100, 0, Math.PI);
+    context.fillStyle = 'rgba(255, 255, 0, 1)';
+    context.fill();
+}
+
 const draw = () => {
     if(!context) return;
 }
+
+draw();
